8cd3494d34fdf2a0ca41c4e2e614e82fc416fd28
BriniM <maherbrini00@gmail.com>
2020-09-21 02:53:15 +0100

    Add friend selection

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,85 @@
 import Messenger from "./messenger";
-import readlineSync from "readline-sync";
+import { question, questionEMail } from "readline-sync";
+import readline from "readline";
+import chalk from 'chalk';
 
+const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: '> '});
 const messengerClient = new Messenger();
 
 (async function () {
-  console.log(`Messenger CLI - 1.0.0`);
+  console.log("Messenger CLI - 1.0.0");
   if (messengerClient.cookiesExist()) {
     await loginWithFile();
-  } else if (!messengerClient.isApiInitialized()) {
+  }
+  if (!messengerClient.isApiInitialized()) {
     do {
-      await loginWithCreds();
+      await loginWithCredentials();
     } while (!messengerClient.isApiInitialized());
   }
-  readlineSync.setDefaultOptions({ prompt: "> " });
-  readlineSync.promptLoop((input: string): boolean => {
-    switch (input) {
-      case "hello":
-        console.log("World!");
+  messengerClient.listen((err: any, event: any) => {
+    switch (event.type) {
+      case "message":
+        messengerClient.api.getUserInfo(event.senderID, (err: any, data: any) => {
+          console.log(`${chalk.blue(data[event.senderID].name)}: ${event.body}`);
+      });
+        break;
+    }
+  });
+  rl.prompt();
+  rl.on('line', input => {
+    input = input.trim();
+    let command;
+    if (input.indexOf(' ') > 0) {
+      command = input.substring(0, input.indexOf(' '));
+    }  else {
+      command = input;
+    }
+
+    switch (command) {
+      case "/m":
+      case "/message":
+        messengerClient.message(input.substring(input.indexOf(' ') + 1));
+        break;
+
+      case "/s":
+      case "/select":
+        if(input.indexOf(' ') > 0) {
+          messengerClient.select(input.substring(input.indexOf(' ') + 1));
+        } else {
+          console.log('Syntax: /s <name> | /select <name>')
+        }
+        break;
+      case "/help":
+        console.log("/help - shows this help page.");
+        console.log(`/select | /s - Select someone to message using their name. 
+                    For example: /s John Doe`);
+        console.log("/message | /m - Sends the text to the selected friend.");
+        console.log("/exit - exits the program.");
         break;
       case "/exit":
         console.log("Farewell!");
-        return true;
+        process.exit(0);
+        break;
       default:
         console.log("Unknown command, see /help");
         break;
     }
-    return false;
+
+    rl.prompt();
   });
 })();
 
 async function loginWithFile() {
-  console.log(`Attempting to login\n`);
+  console.log("Attempting to login");
   await messengerClient.login();
 }
 
-async function loginWithCreds() {
-  console.log(`Attempting to login with email/pass.`);
-  let email = readlineSync.questionEMail("Your email: ");
-  let pass = readlineSync.question("Your password: ", { hideEchoBack: true });
+async function loginWithCredentials() {
+  console.log("Attempting to login with email/pass.");
+  let email = questionEMail("Your email: ");
+  let pass = question("Your password: ", { hideEchoBack: true });
   await messengerClient.login(email, pass);
 }
--- src/messenger.ts
+++ src/messenger.ts
@@ -5,30 +5,63 @@ import login from 'facebook-chat-api';
 
 const loginPromise = promisify(login);
 
+// TODO: Viewing attachments
 export default class Messenger {
   cookiesPath: string;
   api: any;
+  selectedUser: number;
+  private stopListening: any;
   constructor() {  
-	  this.cookiesPath = 'cookies.json';
+	this.cookiesPath = 'cookies.json';
+    this.selectedUser = 0;
   }
 	cookiesExist(): Boolean {
-		return existsSync(this.cookiesPath);
+      return existsSync(this.cookiesPath);
 	}
 
 	getCookies(): Object {
-		return JSON.parse(readFileSync(this.cookiesPath, 'utf8'));
+      return JSON.parse(readFileSync(this.cookiesPath, 'utf8'));
 	}
 
 	saveCookies(): void {
-		writeFileSync(this.cookiesPath, JSON.stringify(this.api.getAppState()));
+      writeFileSync(this.cookiesPath, JSON.stringify(this.api.getAppState()));
 	}
 
 	isApiInitialized(): Boolean {
-		return this.api !== undefined;
+      return this.api !== undefined;
 	}
 
-	async login(email?: string, pass?: string) {
-		if (email == undefined && pass == undefined) {
+  listen(cb: CallableFunction): void {
+    this.stopListening = this.api.listenMqtt(cb);
+  }
+
+  stopListen(): void {
+    this.stopListening();
+  }
+  // TODO: Select and show previous messages
+  select(name: string) {
+    this.api.getUserID(name, (err: any, data: any) => {
+        if (err)
+        {
+          console.error(err);
+        }
+        else {
+          this.selectedUser = data[0].userID;
+          console.log(`Selected ${data[0].name} - ${data[0].userID}`);
+        }
+    });
+}
+
+  message(message: string): void {
+    if(this.selectedUser) {    
+      this.api.sendMessage(message, this.selectedUser);
+    } else {
+      console.log("You need to select a user first using /select, see /help.");
+    }
+  }
+    
+  async login(email?: string, pass?: string) {
+        if (email == undefined && pass == undefined) {
         this.api = await loginPromise({appState: this.getCookies() }).catch((e: any) => { console.log(e) });
     } else {
       this.api = await loginPromise({
